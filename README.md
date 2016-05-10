f54c58a4087646cb2b52b74daacdec3e20b62c67
yume <yume.wang27@gmail.com>
2016-05-10 11:35:18 +0800

    First Commit: Your story begins now

---
+++ README.md
@@ -0,0 +1,30 @@
+# GulpDemo
+
+## A really easy setup
+
+You'll need [git](https://git-scm.com), [nodejs](https://nodejs.org/en/) and [npm](https://docs.npmjs.com) installed. Then you'll need to perform these steps.
+
+```sh
+git clone git@github.com:meanjim/dough.git
+cd dough
+npm install -g gulp
+npm install
+npm start
+```
+
+## Toolchain
+
+tool | reasoning
+------ | -----------
+[Babel](https://babeljs.io/) | EcmaScript2015 (EcmaScript6/7) is worth it to make your code look a little nicer.
+[React](https://facebook.github.io/react/) | Powerful view library with a lot of hype so you have to learn it.
+[React Router](https://github.com/rackt/react-router) | This is a popular tool for client side routing.
+[Redux](http://redux.js.org/) | This is my favorite flux implementation for how to manage state.
+[Gulp](http://gulpjs.com/) | I have used Webpack too much so I switched my tool.
+[SASS](http://sass-lang.com/) | A preprocessor more popular than [LESS](http://lesscss.org/). You could just use normal CSS too.
+[Browserify](http://browserify.org/) | Popular tool for bundling imports/requires.
+[BrowserSync](https://www.browsersync.io/) | External IP Address for testing.
+
+## Tribute Slanging
+
+* [Jimmy Lee](https://github.com/meanJim) is where I got started.
---
+++ app/reducers/root-reducer.js
@@ -0,0 +1,101 @@
+'use-strict';
+
+const initialState = {
+    failure: null,
+    hasData: false,
+    collection: [],
+    selection: []
+}
+
+export function addToSelectionById(id, state) {
+    let collection = [...state.collection];
+    let selection = [...state.selection];
+    let targetItem = null;
+
+    collection = collection.filter((item) => {
+        if (item.id === id) {
+            targetItem = item;
+
+            return false;
+        }
+
+        return true;
+    });
+
+    selection.unshift(targetItem);
+
+    return {
+        ...state,
+        collection,
+        selection
+    };
+}
+
+export function removeFromSelectionById(id, state) {
+    let collection = [...state.collection];
+    let selection = [...state.selection];
+    let targetItem = null;
+
+    selection = selection.filter((item) => {
+        if (item.id === id) {
+            targetItem = item;
+            return false;
+        }
+
+        return true;
+    });
+
+    collection.unshift(targetItem);
+
+    return {
+        ...state,
+        collection,
+        selection
+    };
+}
+
+export function addDataToCollection(collection, state) {
+    let hash = {};
+
+    const joinedCollection = state.collection.concat(collection).filter((item) => {
+        return hash.hasOwnProperty(item.id) ? false : (hash[item.id] = true);
+    });
+
+    return {
+        ...state,
+        collection: joinedCollection,
+        hasData: true
+    };
+}
+
+export function showErrorMessage(message, state) {
+    console.log(message)
+    return {
+        ...state,
+        failure: message
+    }
+}
+
+export function removeErrorMessage(state) {
+    return {
+        ...state,
+        failure: null
+    }
+}
+
+export function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TO_YOUR_LIST':
+            return addToSelectionById(action.id, state);
+        case 'REMOVE_FROM_YOUR_LIST':
+            return removeFromSelectionById(action.id, state);
+        case 'ADD_GIF_COLLECTION':
+            return addDataToCollection(action.collection, state);
+        case 'NETWORK_FAILURE':
+            return showErrorMessage(action.failure, state);
+        case 'REMOVE_NETWORK_FAILURE':
+            return removeErrorMessage(state);
+        default:
+            return state;
+    }
+}
